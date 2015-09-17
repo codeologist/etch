d2233bfb3ab135154463251b874a55c664b81a88
tony <tony.gurnick@gmail.com>
2015-09-18 00:53:47 +0100

    basic test framework setup

---
+++ test/empty.js
@@ -0,0 +1,14 @@
+
+    "use strict";
+
+    var assert = require('assert');
+
+    describe('Empty Test', function(){
+
+
+        it('assert true', function(done){
+            assert(true);
+            done();
+        });
+
+    });
